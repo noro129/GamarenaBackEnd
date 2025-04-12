d4b164e960dd3df236ddb84173fa44e37697b6dc
ERRAZI <errazi111@gmail.com>
2025-04-12 19:33:32 +0100

    initial commit

---
+++ src/main/java/com/gamarena/GameArenaBackend/GameArenaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gamarena.GameArenaBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameArenaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameArenaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamarena/GameArenaBackend/GameArenaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamarena.GameArenaBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameArenaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
